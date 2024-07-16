d3c5e1ea70fef24a4b807e1abc5ff114cf04f00c
arshmanahmad <132290386+arshmanahmad@users.noreply.github.com>
2024-07-16 02:47:45 -0500

    update

---
+++ src/components/ArshWhizTable/index.tsx
@@ -0,0 +1,45 @@
+import React from "react"
+
+interface TableProps {
+    arrayOfData: { [key: string]: any }[],  // Define the type for arrayOfData
+    attributesToShow: string[],
+    attributesNames: string[],
+    title?: string,
+    showButton?: boolean,
+    onRecordClick?: (record: { [key: string]: any }) => void, // Define as a function
+    onButtonClick?: () => void,
+}
+
+const Table: React.FC<TableProps> = ({
+    attributesToShow,
+    attributesNames,
+}) => {
+    return (
+        <>
+            <div>
+                <table>
+                    <thead>
+                        {!attributesToShow && !attributesNames ?
+                            <>
+                            </> :
+                            <>
+                                {attributesToShow ? attributesToShow.map((item) => {
+                                    return <th>{item}</th>
+                                })
+                                    : <>
+                                        {attributesNames.map((item) => {
+                                            return <th>{item}</th>
+                                        })}
+                                    </>}
+                            </>
+                        }
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+        </>
+    )
+}
+export default Table
